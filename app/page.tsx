61feb4911f62b02546dbdb05653b5481d8723fa6
Gary Handasis <awmanagment@gmail.com>
2024-02-14 18:05:05 +0200

    tre2

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>Gary has taken over</div>
+      <div>Gary has taken over tryr 2</div>
     </main>
   )
 }
